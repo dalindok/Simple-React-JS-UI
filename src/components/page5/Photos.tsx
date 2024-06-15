4dec968349557d5a78aa19ea45106d331d526c3c
dalindok <dalindok211@gmail.com>
2024-06-15 11:38:09 +0700

    update page 5

--- src/components/page5/Photos.tsx
+++ src/components/page5/Photos.tsx
@@ -4,18 +4,28 @@ import Second from "../../assets/Second.png";
 import Third from "../../assets/Third.png";
 function Photos() {
   return (
-    <div className="flex flex-row h-screen ">
-      <div className="flex flex-col h-full justify-between">
-        <div className="w-72 relative ">
+    <div className="flex flex-row h-screen p-5">
+      <div className="">
+        <div className="w-72 h-2/5 relative">
           <p className="absolute top-5 left-5 font-bold text-lg">04</p>
-          <img src={First} alt="04" className="" />
+          <img
+            src={First}
+            alt="04"
+            className=""
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
         </div>
-        <div className="w-72 relative">
+        <div className="w-72 h-3/5 relative mt-5">
           <p className="absolute top-5 left-5 font-bold text-lg">05</p>
-          <img src={Second} alt="05" className="" />
+          <img
+            src={Second}
+            alt="05"
+            className=""
+            style={{ width: "100%", height: "94%", objectFit: "cover" }}
+          />
         </div>
       </div>
-      <div className="flex w-full h-full ml-6 relative">
+      <div className="flex ml-6 relative">
         <p className="absolute top-5 left-5 font-bold text-lg">06</p>
         <img src={Third} alt="06" className="" />
       </div>
--- src/pages/Page5.tsx
+++ src/pages/Page5.tsx
@@ -3,7 +3,7 @@ import Photos from "../components/page5/Photos";
 import Text from "../components/page5/Text";
 function Page5() {
   return (
-    <div className="flex flex-row justify-between p-5">
+    <div className="flex flex-row justify-between">
       <Text />
       <Photos />
     </div>
