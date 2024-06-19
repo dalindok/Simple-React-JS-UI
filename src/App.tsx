388ff4ec16134dc0b27ff5ae79625106aac2aa39
Thonida1045 <ithonida@gmail.com>
2024-06-19 15:15:40 +0700

    update page3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Part1 from "./components/page3/Part1";
+//import Part1 from "./components/page3/Part1";
+import Page3 from './pages/Page3';
 
 function App() {
   return (
     <div className="App">
      <div className="App-header">
-      <Part1/>
+      <Page3/>
      </div>
     </div>
   );
