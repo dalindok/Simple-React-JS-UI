708d1325c903e2d867ea1fe91accbb03df456efe
Thonida1045 <ithonida@gmail.com>
2024-06-19 15:45:08 +0700

    update page4

---
+++ src/pages/Page4.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import image01 from '../assets/page4.jpg'
+
+function Page4() {
+    return (
+        <div className='relative'>
+            <div className='absolute top-28 left-70 ml-32 text-white flex flex-col'>
+                <p className='text-3xl'>"Jasmine has a unique </p>
+                <p className='text-3xl'>style and is a true</p>
+                <p className='text-3xl'>professional."</p>
+                <p className='text-xs pt-4'>- Shiloh Carson, Client</p>
+            </div>
+            <img src={image01} alt="page4" className='h-screen w-full'/>
+        </div>
+    );
+}
+
+export default Page4;
